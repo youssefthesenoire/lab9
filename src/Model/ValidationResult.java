62bfe8f6b3e3588ce9f5791d8fb854be5d6f7436
Kouka <mohamedhassansabbagh2005@gmail.com>
2025-11-29 17:39:58 +0200

    Add ValidationResult class for validation results

---
+++ src/Model/ValidationResult.java
@@ -0,0 +1,21 @@
+package Model;
+
+import java.util.List;
+
+public class ValidationResult {
+    private final boolean valid;
+    private final List<LocationOnBox> duplicateLocations;
+    
+    public ValidationResult(boolean valid, List<LocationOnBox> duplicateLocations) {
+        this.valid = valid;
+        this.duplicateLocations = duplicateLocations;
+    }
+    
+    public boolean isValid() {
+        return valid;
+    }
+    
+    public List<LocationOnBox> getDuplicateLocations() {
+        return duplicateLocations;
+    }
+}
