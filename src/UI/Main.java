04443bb39fc0510751ba73660a61ad6b3e0252a6
sketlafox <es.ahmed.bakr2023@alexu.edu.eg>
2025-11-30 01:53:20 +0200

    main

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -1,12 +1,11 @@
 package UI;
 
-import Model.Column;
-import Model.Row;
-import Model.Table;
-import Model.ValidationResult;
+import Model.*;
 import Service.Mode_Three;
 import Service.Mode_TwentySeven;
 import Service.Mode_Zero;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,38 +16,114 @@ public class Main {
 
         String csvPath = args[0];  // first argument
         String mode = args[1];     // second argument
-
+//        String path = "C:\\Users\\ahmed\\Desktop\\New Text Document (2).csv";
         System.out.println("CSV Path: " + csvPath);
         System.out.println("Mode: " + mode);
-        Table t=Table.getTable(args[1]);
-        Row[]r=t.getRows();
-        Column[]c=t.getColumns();
-        for(Row row:r){
-            
+        Table t = Table.getTable(csvPath);
+        Row[] r = t.getRows();
+        Column[] c = t.getColumns();
+        Box[] b = t.getBoxes();
+        StringBuilder s = new StringBuilder();
+
+        for (int i = 0; i < r.length; i++) {
+            for (int j = 0; j < c.length; j++) {
+                int value = r[i].getRowElements()[j];
+                if (j != 0 && j % 3 == 0) {
+
+                    s.append("*");
+                    s.append("     " + value + "     ");
+
+                } else {
+
+                    s.append(value + "     ");
+                }
+
+            }
+            if (i != 0 && (i + 1) % 3 == 0 && (i + 1) != 9) {
+
+                s.append("\n");
+                s.append("**************************************************************\n");
+
+            } else {
+                s.append("\n");
+                s.append("------------------*-----------------------*------------------\n");
+            }
         }
 
 
+        System.out.println(s);
+
+
         ValidationResult v = null;
+        String o="null";
 
         switch (mode) {
             case "Method0":
                 v = Mode_Zero.validate(t);
+                o = "#1";
                 break;
             case "Method3":
                 v = Mode_Three.validate(t);
+                o = "#2";
                 break;
             case "Method27":
                 v = Mode_TwentySeven.validate(t);
+                o = "#3";
                 break;
             default:
                 System.out.println("Unknown mode: " + mode);
+
+
                 return;
         }
+        StringBuilder out = new StringBuilder();
 
-        if (!v.isValid()) {
-            System.out.println("Invalid data found!");
-        } else {
-            System.out.println("Data is valid.");
+        if (v != null) {
+            if (v.isValid()) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+
+            }
+
+            for (int i = 0; i < r.length; i++) {
+                out.append("Row" + (i + 1) + "," + o + ",[");
+                int[] rowElements = r[i].getRowElements();
+                for (int j = 0; j < rowElements.length; j++) {
+                    out.append(rowElements[j]);
+                    if (j < rowElements.length - 1) {
+                        out.append(",");
+                    }
+                }
+                out.append("]\n");
+            }
+            out.append("------------------------------------------------------------\n");
+            for (int i = 0; i < c.length; i++) {
+                out.append("Column" + (i + 1) + "," + o + ",[");
+                int[] columnElements = c[i].getcolumnElements();
+                for (int j = 0; j < columnElements.length; j++) {
+                    out.append(columnElements[j]);
+                    if (j < columnElements.length - 1) {
+                        out.append(",");
+                    }
+                }
+                out.append("]\n"); // newline after each column
+            }
+            out.append("------------------------------------------------------------\n");
+            for (int i = 0; i < b.length; i++) {
+                out.append("Box" + (i + 1) + "," + o + ",[");
+                int[] boxElements = b[i].getBoxElements();
+                for (int j = 0; j < b.length; j++) {
+                    out.append(boxElements[j]);
+                    if (j < boxElements.length - 1) {
+                        out.append(",");
+                    }
+                }
+                out.append("]\n");
+
+            }
+
+            System.out.println(out);
         }
     }
 }
