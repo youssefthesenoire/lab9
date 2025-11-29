da570c44a2992a50bcb502a9b19010fd1032b556
Kouka <mohamedhassansabbagh2005@gmail.com>
2025-11-29 17:35:12 +0200

    Refactor LocationOnBox class with getters and overrides

--- src/Model/LocationOnBox.java
+++ src/Model/LocationOnBox.java
@@ -1,10 +1,37 @@
 package Model;
 
 public class LocationOnBox {
-    private int x,y;
-    public LocationOnBox(int x,int y)
-    {
+    private int x;
+    private int y;
+    
+    public LocationOnBox(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+    
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
+    }
+    
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        LocationOnBox that = (LocationOnBox) obj;
+        return x == that.x && y == that.y;
+    }
+    
+    @Override
+    public int hashCode() {
+        return 31 * x + y;
     }
 }
