923ed84337265127f9a5d3eff2a361b2ac8bcef0
Kouka <mohamedhassansabbagh2005@gmail.com>
2025-11-29 17:37:15 +0200

    Refactor validate method to return ValidationResult

--- src/Service/Mode_Zero.java
+++ src/Service/Mode_Zero.java
@@ -3,44 +3,47 @@ package Service;
 import Model.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Mode_Zero {
-    public static boolean validate(ArrayList<LocationOnBox> locations)
-    {
-        Table t = Table.getTable();
-        boolean  valid = true;
-        for (int i= 0; i < 9; i++) {
-            HashMap<Integer, ArrayList<Integer>> dup = t.getRows()[i].getDuplicatedRow();
-            for (int num : dup.keySet()) {
-                ArrayList<Integer> columns = dup.get(num);
-                for (int c : columns) {
-                    locations.add(new LocationOnBox(i, c));
-                    valid = false;
+    public static ValidationResult validate() {
+        Table table = Table.getTable();
+        Set<LocationOnBox> duplicateLocations = new HashSet<>();
+        boolean isValid = true;
+        
+        for(int i = 0; i < 9; i++) {
+            HashMap<Integer, ArrayList<Integer>> rowDup = table.getRows()[i].getDuplicatedRow();
+            for(int num : rowDup.keySet()) {
+                ArrayList<Integer> columns = rowDup.get(num);
+                for(int col : columns) {
+                    duplicateLocations.add(new LocationOnBox(i, col));
+                    isValid = false;
                 }
             }
         }
-        for (int i= 0; i < 9; i++) {
-            HashMap<Integer, ArrayList<Integer>> dup = t.getColumns()[i].getDuplicatedColumn();
-            for (int num : dup.keySet()) {
-                ArrayList<Integer> rows = dup.get(num);
-                for (int r : rows) {
-                    locations.add(new LocationOnBox(i, r));
-                    valid = false;
+        
+        for(int i = 0; i < 9; i++) {
+            HashMap<Integer, ArrayList<Integer>> colDup = table.getColumns()[i].getDuplicatedColumn();
+            for(int num : colDup.keySet()) {
+                ArrayList<Integer> rows = colDup.get(num);
+                for(int row : rows) {
+                    duplicateLocations.add(new LocationOnBox(row, i));
+                    isValid = false;
                 }
             }
         }
-        for (Box box : t.getBoxes()) {
-            HashMap<Integer, ArrayList<LocationOnBox>> dup = box.getDuplicatedBox();
-            for (int num : dup.keySet()) {
-                ArrayList<LocationOnBox> locs = dup.get(num);
-                for (LocationOnBox loc : locs) {
-                    locations.add(loc);
-                    valid = false;
-                }
+        
+        for(Box box : table.getBoxes()) {
+            HashMap<Integer, ArrayList<LocationOnBox>> boxDup = box.getDuplicatedBox();
+            for(int num : boxDup.keySet()) {
+                ArrayList<LocationOnBox> locations = boxDup.get(num);
+                duplicateLocations.addAll(locations);
+                isValid = false;
             }
         }
-        return valid;
+        
+        return new ValidationResult(isValid, new ArrayList<>(duplicateLocations));
     }
 }
